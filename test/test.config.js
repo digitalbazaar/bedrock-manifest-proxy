e9cdb351dde214a5e096bd39f5640812ff8e694e
Matthew Collier <mcollier@digitalbazaar.com>
2020-06-18 17:43:26 -0400

    Use bedrock-karma dev branch and update config.

--- test/test.config.js
+++ test/test.config.js
@@ -15,3 +15,14 @@ config.karma.config.proxies = {
   '/': 'https://localhost:18443'
 };
 config.karma.config.proxyValidateSSL = false;
+
+config.karma.config.customLaunchers = {
+  Chrome_without_security: {
+    base: 'ChromeHeadless',
+    flags: [
+      // '--disable-web-security',
+      '--ignore-ssl-errors',
+      '--ignore-certificate-errors'
+    ]
+  }
+};
